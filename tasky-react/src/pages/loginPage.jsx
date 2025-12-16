3e59e1d6d09b4adb234e0dd2ca0f8b9a0edee394
KyleG4 <20107489@setu.ie>
2025-12-16 18:08:05 +0000

    Authentication Context and Login Page

--- tasky-react/src/pages/loginPage.jsx
+++ tasky-react/src/pages/loginPage.jsx
@@ -1,12 +1,35 @@
+import { useContext, useState } from "react";
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from '../contexts/authContext';
 import { Link } from "react-router";
+
 const LoginPage = () => {
+    const context = useContext(AuthContext);
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        context.authenticate(userName, password);
+    };
+
+    let location = useLocation();
+    const { from } = location.state ? { from: location.state.from.pathname } : { from: "/" };
+
+    if (context.isAuthenticated === true) {
+        return <Navigate to={from} />;
+    }
+
     return (
         <>
             <h2>Login page</h2>
             <p>You must log in to view the protected pages </p>
-            <input id="username" placeholder="user name"></input><br />
-            <input id="password" type="password" placeholder="password"></input><br />
-            <button>Log in</button>
+            <input id="username" placeholder="user name" onChange={e => {
+                setUserName(e.target.value);
+            }}></input><br />
+            <input id="password" type="password" placeholder="password" onChange={e => {
+                setPassword(e.target.value);
+            }}></input><br />
+            <button onClick={login}>Log in</button>
             <p>Not Registered?
                 <Link to="/signup">Sign Up!</Link></p>
         </>
